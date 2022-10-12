65aee33db25ac540d83c4ef1e1fbdbb3b7ed8d92
mkaan <mkaan@GTRYERMISINSNOT>
2022-10-12 20:31:11 +0300

    Add project files.

---
+++ YouTubeEgitim/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace YouTubeEgitim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            sayi1 = sayi2;
+            sayi2 = 100;
+            Console.WriteLine(sayi1);
+
+            int[] sayilar1 = new[] { 1, 2, 3 };
+            int[] sayilar2 = new[] { 10, 20, 30 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 1000;
+            Console.WriteLine(sayilar1[0]);
+
+            //IoC Container
+            CustomerManager customerManager = new CustomerManager(new Customer(), new TeacherCreditManager());
+            customerManager.GiveCredit();
+        }
+    }
+
+    class CreditManager
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Hesaplandı");
+        }
+        public void Save()
+        {
+
+            Console.WriteLine("Kredi verildi");
+        }
+    }
+    interface ICreditManager
+    {
+        void Calculate();
+        void Save();
+    }
+
+    abstract class BaseCreditManager : ICreditManager
+    {
+        public abstract void Calculate();
+
+        public virtual void Save()
+        {
+            Console.WriteLine("kaydedildi");
+        }
+    }
+
+    class TeacherCreditManager : BaseCreditManager, ICreditManager
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("asker kredisi hesaplandı");
+        }
+
+        public override void Save()
+        {
+            base.Save();
+        }
+    }
+
+    class MilitaryCreditManager : BaseCreditManager, ICreditManager
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Asker kredisi hesaplandı");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("kaydedildi");
+        }
+    }
+    //SOLID 
+    class Customer
+    {
+        public Customer()
+        {
+            Console.WriteLine("müşteri nesnesi başlatıldı");
+        }
+
+        public int Id { get; set; }
+        public string City { get; set; }
+    }
+    class Person:Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentity { get; set; }
+    }
+
+    class Company:Customer
+    {
+        public string CompanyName { get; set; }
+        public string TaxNumber { get; set; }
+    }
+
+    //Katmanlı Mimariler
+    class CustomerManager
+    {
+        private Customer _customer;
+        private ICreditManager _creditManager;
+        public CustomerManager(Customer customer, ICreditManager creditManager)
+        {
+            _customer = customer;
+            _creditManager = creditManager;
+        }
+        public void Save()
+        {
+
+            Console.WriteLine("Müşteri Kaydedildi: ");
+        }
+        public void Delete()
+        {
+
+            Console.WriteLine("Müşteri Silindi: ");
+        }
+
+        public void GiveCredit()
+        {
+            _creditManager.Calculate();
+            Console.WriteLine("kredi verildi");
+        }
+    }
+}
